2527f197fb3fece05c69cb9cf56410ee7249f7e9
Nikita <ntrafilkin88@gmail.com>
2020-02-11 11:08:24 +0300

    adds simple UI

--- src/index.js
+++ src/index.js
@@ -6,15 +6,28 @@ const reducer = (state = 0, action) => {
       return state + 1;
     case "DEC":
       return state - 1;
+    case "RND":
+      return state + action.payload;
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
+
+document.getElementById("inc").addEventListener("click", () => {
+  store.dispatch({ type: "INC" });
 });
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "DEC" });
+document.getElementById("dec").addEventListener("click", () => {
+  store.dispatch({ type: "DEC" });
+});
+document.getElementById("rnd").addEventListener("click", () => {
+  const payload = Math.floor(Math.random() * 10);
+  store.dispatch({ type: "RND", payload });
+});
+
+const update = () => {
+  document.getElementById("counter").innerHTML = store.getState();
+};
+
+store.subscribe(update);
