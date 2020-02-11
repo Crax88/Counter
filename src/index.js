8e9cdf3e190a4719f33d7c342f204197e7f79055
Nikita <ntrafilkin88@gmail.com>
2020-02-11 10:38:15 +0300

    basic redux implementaion - counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const initialState = 0;
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+let state = reducer(initialState, { type: "INC" });
+console.log(state);
+state = reducer(state, { type: "INC" });
+console.log(state);
+state = reducer(state, { type: "DEC" });
+console.log(state);
